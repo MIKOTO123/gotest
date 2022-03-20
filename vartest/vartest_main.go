c38b074752e3479d87521edc3c8adea85ac65dee
pikazo <pikazo512@gmail.com>
2022-03-20 20:36:02 +0800

    提交关于go变量部分的剩下的代码

--- vartest/vartest_main.go
+++ vartest/vartest_main.go
@@ -7,6 +7,8 @@ import (
 	"vartest/commonutils"
 ) //这里应用的包名,试用go mod之后, 是用go.mod文件下面module vartest 这个包做为基础的报名,
 
+var name = "haha"
+
 func main() {
 
 	//Test1() //这里如果要Test1可用的话,必须是 "go run vartest_main.go vartest1.go"   ,两个文件一起包含,然而下面commonutils.Toupper却不需要
@@ -17,8 +19,34 @@ func main() {
 
 	testConfig()
 
+	fmt.Printf("%v\n", name) //局部函数里面定义的,,,不会覆盖全局变量的name
+	name = "smgui"
+	fmt.Printf("%v\n", name)
+
 }
 
+func getnNameAndAge() (string, int) {
+	return "pipi", 123
+}
+
+func test5() {
+	name := "pikazo"
+	age := 190
+	email := "smgui@smgui.com"
+
+	fmt.Printf("name:%v\n", name)
+	fmt.Printf("age:%v\n", age)
+	fmt.Printf("email:%v\n", email)
+
+}
+func test4() {
+
+	var sex = 1
+	var name, age = "pikazo", 12
+	fmt.Printf("name:%v\n", name)
+	fmt.Printf("age:%v\n", age)
+	fmt.Printf("sex:%v\n", sex)
+}
 func test3() {
 	commonfunc.PrintStartInFunc3()
 	var name string = "wo shinidie"
